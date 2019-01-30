95994535970fc3e4ca5025dc4de4846a6c53dbc8
YogiKiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2019-01-30 11:56:58 +0900

    EffectCreaterScriptの作成

---
+++ ProjectMilk/Assets/Yogi/EffectCreaterScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectCreaterScript : MonoBehaviour {
+    public string[] EffectKeyArray;
+    public GameObject[] EffectGameObjectArray;
+    public Dictionary<string, GameObject> Effect_Array=new Dictionary<string, GameObject>();
+    Vector2 TestPos = new Vector2(0, 0);
+	// Use this for initialization
+	void Start () {
+        for (int i=0; (i < 100) && (EffectKeyArray[i] != null);i++ )
+        {
+            Effect_Array.Add(EffectKeyArray[i],EffectGameObjectArray[i]);
+        }
+	}
+	
+	// Update is called once per fr
+	void Update () {
+        if(Input.GetMouseButtonUp(0)){
+            EfectCreate(TestPos,"Bomb");
+        }
+		
+	}
+
+    void EfectCreate(Vector2 Pos,string Key)
+    {
+       
+      Instantiate(Effect_Array[Key]);
+                
+    }
+}
