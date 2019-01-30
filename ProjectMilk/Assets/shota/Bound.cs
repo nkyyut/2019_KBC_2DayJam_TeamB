8d570956dcda6aabdec391c242d4e77704d9d58a
yonahasi0911 <shota-y-17d@stu.kbc.ac.jp>
2019-01-30 13:04:05 +0900

    反射するScriptを作りました

---
+++ ProjectMilk/Assets/shota/Bound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bound : MonoBehaviour {
+
+    //GameObject Player;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Vector2 refrectVec = Vector2.Reflect(col.gameObject.GetComponent<PLCon_shota>().GetVec(), col.contacts[0].normal);
+
+            col.gameObject.GetComponent<PLCon_shota>().SetVec(refrectVec);
+        }
+    }
+}
---
+++ ProjectMilk/Assets/shota/PLCon_shota.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLCon_shota : BasePlayer
+{
+
+    [SerializeField] Vector2 myVec;
+
+    void Start()
+    {
+        base.PlayerSpeed = 200.0f;
+        base.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(myVec * Time.deltaTime, 0);
+    }
+
+    public void SetVec(Vector2 v)
+    {
+        myVec = v;
+    }
+
+    public Vector2 GetVec()
+    {
+        return myVec;
+    }
+}
