c3b167da4902dcc256bb0649d5436f695cd6350c
YogiKiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2019-01-30 12:49:16 +0900

    EffectCreaterScriptをサイズ変更できるようにした

--- ProjectMilk/Assets/Yogi/EffectCreaterScript.cs
+++ ProjectMilk/Assets/Yogi/EffectCreaterScript.cs
@@ -9,7 +9,7 @@ public class EffectCreaterScript : MonoBehaviour {
     Vector2 TestPos = new Vector2(0, 0);
 	// Use this for initialization
 	void Start () {
-        for (int i=0; (i < 100) && (EffectKeyArray[i] != null);i++ )
+        for (int i=0; EffectKeyArray.Length>i;i++ )
         {
             Effect_Array.Add(EffectKeyArray[i],EffectGameObjectArray[i]);
         }
@@ -18,15 +18,15 @@ public class EffectCreaterScript : MonoBehaviour {
 	// Update is called once per fr
 	void Update () {
         if(Input.GetMouseButtonUp(0)){
-            EfectCreate(TestPos,"Bomb");
+            EfectCreate(TestPos,"Bomb",0.5f);
         }
 		
 	}
 
-    void EfectCreate(Vector2 Pos,string Key)
+    void EfectCreate(Vector2 Pos,string Key,float EffectSize)
     {
-       
-      Instantiate(Effect_Array[Key]);
-                
+        GameObject Go;
+        Go = Instantiate(Effect_Array[Key])as GameObject;
+        Go.transform.localScale = new Vector3(EffectSize,EffectSize,EffectSize);
     }
 }
