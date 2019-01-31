579874d0d8bde4a7135ca8e870ec5095e8f5c004
DESKTOP-0ARPSR5\jin <tamanaha1205@gmail.com>
2019-01-31 12:34:59 +0900

    敵がやられたときに画像変更処理追加

--- ProjectMilk/Assets/Jin/EnemyController.cs
+++ ProjectMilk/Assets/Jin/EnemyController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
+    public enum SpriteType // 画像数を加減する場合はこの列挙定数を変更してください
+    {
+        FIRST,
+        SECOND,
+    }
+    [SerializeField] private SpriteType MyType = SpriteType.FIRST; // 画像格納配列の添字
+
+    [SerializeField] private Sprite[] sprites; // 画像を格納する配列(インスペクタで格納してください)
+
+    private SpriteRenderer Sprd; // 自オブジェクトのコンポーネントへアクセスするための変数
+
     Rigidbody2D rigid;
 
     private float   Smash_power     = 500.0f;
@@ -14,6 +25,7 @@ public class EnemyController : MonoBehaviour {
 
     private void Start()
     {
+        Sprd = GetComponent<SpriteRenderer>();
         rigid = GetComponent<Rigidbody2D>();
         Smash_offset.y = 1;
     }
@@ -25,6 +37,12 @@ public class EnemyController : MonoBehaviour {
         if(this.transform.position.y < -5.3f)Destroy(this.gameObject);
         if(this.transform.position.y > 5.32f)Destroy(this.gameObject);
         
+        Sprd.sprite = sprites[(int)MyType];
+    }
+
+    public void SetSprite(int type)// 1～N（N=2以上の自然数)をゼロベースに直し添字に代入
+    {
+        MyType = (SpriteType)type - 1;
     }
 
     public void SmashEnemy()
@@ -33,5 +51,7 @@ public class EnemyController : MonoBehaviour {
         Smash_powerDir.y = Random.Range(-1.0f, 1.0f);
         Smash_powerDir.x = Random.Range(-1.0f, 1.0f);
         rigid.AddForceAtPosition( Smash_powerDir.normalized * Smash_power, transform.position + Smash_offset );
+
+        MyType++;
     }
 }
