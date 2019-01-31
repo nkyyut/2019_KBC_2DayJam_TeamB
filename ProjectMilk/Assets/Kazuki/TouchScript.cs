21b8ab88f631ec64e274d6fe2fedd9f8e5aa3d92
kazuki24 <kazu2324ki@gmail.com>
2019-01-31 17:02:00 +0900

    タイトルの処理作成しました

---
+++ ProjectMilk/Assets/Kazuki/TouchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchScript : MonoBehaviour {
+
+    AudioSource audio;
+    AudioSource audio2;
+
+    float Time = 0f;
+    private bool SceneFlg = false;
+
+	// Use this for initialization
+	void Start () {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        audio = audioSources[0];
+        audio2 = audioSources[1];
+        audio2.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(SceneFlg == true)
+             if (!audio.isPlaying)
+                   SceneManager.LoadScene("AngleSecen");    //メインのシーン名を
+    }
+    public void OnClick()
+    {
+        audio.PlayOneShot(audio.clip);
+        SceneFlg = true;
+    }
+}
